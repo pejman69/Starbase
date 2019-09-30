a8187e8a2c2a2a5809b6ea5710af58456e52390f
pejman <pejmanmowloudi@gmail.com>
2019-09-30 15:04:22 +0330

    create style and get items

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="" @click="switchCharacter">
-    {{character}}
+<div class="col-md-4" @click="switchCharacter">
+  <div class="character-card">
+    <div class="card-block">
+      <h4 class="card-title">{{character.name}}</h4>
+      <p class="card-text">Height: {{character.height}}cm</p>
+      <p class="card-text">Mass: {{character.mass}}kg</p>
+      <p class="card-text">Hair color: {{character.hair_color}}</p>
+      <p class="card-text">Eye color: {{character.eye_color}}</p>
+    </div>
   </div>
+</div>
 </template>
 <script>
 export default {
   props: ['id'],
-  data(){
+  data() {
     return {
       character: {}
     }
@@ -14,17 +22,16 @@ export default {
   methods: {
     fetchCharacter(id) {
       fetch(`https://swapi.co/api/people/${id}`, {
-        method: 'GET'
-      })
-      .then(response => response.json())
-      .then(json => this.character = json)
+          method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => this.character = json)
     },
-  switchCharacter()
-  {
-    let random_id = Math.floor(Math.random() * 83) + 1
-    this.fetchCharacter(random_id)
-  }
-},
+    switchCharacter() {
+      let random_id = Math.floor(Math.random() * 83) + 1
+      this.fetchCharacter(random_id)
+    }
+  },
   created() {
     this.fetchCharacter(this.id)
   }
